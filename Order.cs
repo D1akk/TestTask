0cedd380b4251739149aa7c9cddb8505d61b9aba
egor dyakonov <egordyakonov2000@gmail.com>
2024-03-01 00:56:01 +0900

    Добавьте файлы проекта.

---
+++ Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    internal class Order
+    {
+        public int id { get; set; }
+        public Coordinate pointA { get; set; }
+        public Coordinate pointB { get; set; }
+        public double price { get; set; }
+
+    }
+}
